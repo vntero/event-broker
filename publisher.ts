e4b0f612d4365e23287eb59f6f49e27b67081f02
vntero <shin.antero@gmail.com>
2022-03-28 19:48:30 +0200

    declared a queue and published to it

--- publisher.ts
+++ publisher.ts
@@ -3,10 +3,24 @@ import amqp from 'amqplib';
 
 //establish a CONNECTION to the server
 amqp.connect('amqp://localhost', function(error0, connection) {
-    
+    if (error0) {
+        throw error0;
+    }
+    //create a CHANNEL, which is where most of the api for getting things done resides
+    connection.createChannel(function(error1, channel) {
+    //declare a QUEUE for us to send to
+    let queue = "hello"
+    let msg = "hi, mom!"
+    //then PUBLISH a message to the queue    
+    channel.assertQueue(queue, {
+        durable:false
+    })
+    channel.sendToQueue(queue, Buffer.from(msg))
+    console.log("[x] Sent %s", msg)
+    })
 })
 
-//create a CHANNEL, which is where most of the api for getting things done resides
+
 
 
 
