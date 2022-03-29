d48bc2f7d2e645d00da4694395185911388912e5
vntero <shin.antero@gmail.com>
2022-03-29 16:19:28 +0200

    corrected minor bugs, should run now

--- src/consumer.ts
+++ src/consumer.ts
@@ -3,24 +3,21 @@ const amqp = require('amqplib/callback_api')
 
 //connect to server
 amqp.connect('amqp://localhost', function(error0, connection) {
-    if (error0) {
-        throw error0
-    }
+    if (error0) {throw error0}
     //create channel
     connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1
-        }
+        if (error1) {throw error1}
         //specify the name of the queue we'll be getting our message from
         let queue = 'crypto'
-
-        channel.assertQueue(queue, {
-            durable: true
-        })
-        console.log('[*] Waiting for messages in &s. To exit press CTRL+C', queue);
+        //go check the QUEUE
+        channel.assertQueue(queue, {durable: true})
+        //prefetch the channel
+        channel.prefetch(1)
+        console.log("[*] Waiting for messages in %s. To exit press CTRL+C", queue)
         //consume the message that is waiting in the queue
         channel.consume(queue, function(msg) {
             var secs = msg.content.stoString().split('.').length - 1
+
             console.log('[x] Received %s', msg.content.toString())
             setTimeout(function() {
                 console.log(" [x] Done")
--- src/publisher.ts
+++ src/publisher.ts
@@ -5,27 +5,23 @@ const amqp = require('amqplib/callback_api');
 
 //establish a CONNECTION to the server
 amqp.connect('amqp://localhost', function(error0, connection) {
-    if (error0) {
-        throw error0;
-    }
+        if (error0) {throw error0}
     //create a CHANNEL, which is where most of the api for getting things done resides
     connection.createChannel(function(error1, channel) {
+        if (error1) {throw error1}
     //declare a QUEUE for us to send to
     let queue = "crypto"
+    //what's the MESSAGE?
     let msg = process.argv.slice(2).join(' ') || "Bitcoin is freedom! Ethereum is decentralisation!"
-    //then PUBLISH a message to the queue    
-    channel.assertQueue(queue, {
-        durable: true
-    })
-    channel.sendToQueue(queue, Buffer.from(msg), {
-        persistent: true
-    })
+    //then PUBLISH a message to the queue and console.log it
+    channel.assertQueue(queue, {durable: true})
+    channel.sendToQueue(queue, Buffer.from(msg), {persistent: true})
     console.log("[x] Sent %s", msg)
     //CLOSE the connection and EXIT
-    // setTimeout(function() {
-    //     connection.close()
-    //     process.exit(0)
-    // }, 500)
+    setTimeout(function() {
+        connection.close()
+        process.exit(0)
+    }, 500)
     })
 })
 
