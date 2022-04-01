9649133803865802401f5a88615fabaf22e6e799
vntero <shin.antero@gmail.com>
2022-04-01 11:50:17 +0200

    advancing on writing the tests

--- test/publish.test.js
+++ test/publish.test.js
@@ -1,5 +1,12 @@
+//FIRST we require the unit we want to test
 const publish = require('./src/consume')
 
+//SECOND we write the test
+//the first argument of the test method is a DESCRIPTION string
+//the second one is the TEST itself, an anonymous function where we will write what we expect
 test (
-
+    "Here we will test if we are able to SEND our messages to the exchange",
+    () => {
+        
+    }
 )
