cbd3f8114797abad70d84d3544683fc5eeab487c
vntero <shin.antero@gmail.com>
2022-04-07 15:21:40 +0200

    ackowledgement working as expected

--- src/receiver.js
+++ src/receiver.js
@@ -14,13 +14,14 @@ amqp.connect('amqp://localhost', function(error1, connection) {
         if (error2) {throw error2}
         channel.assertExchange(exchange, 'direct', {durable: true})
         
-        channel.assertQueue('', {exclusive: false}, function(error3, q) {
+        channel.assertQueue('european', {exclusive: false}, function(error3, q) {
             if (error3) {throw error3}
             console.log(' [x] Waiting for logs. To exit press CTRL+C')
             args.forEach(function(severity) {channel.bindQueue(q.queue, exchange, severity)})
             channel.consume(q.queue, function(incomingMsg) {
                 console.log(" [x] %s:'%s'", incomingMsg.fields.routingKey, incomingMsg.content.toString())
-            }, {Ack: false})
+                channel.ack(incomingMsg)
+            })
         })
     })
 })
